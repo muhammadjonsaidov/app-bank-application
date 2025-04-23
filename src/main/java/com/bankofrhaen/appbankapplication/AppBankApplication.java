b75931108dbe7e28e619a0a6e18e283648c41d01
muhammadjonsaidov <javascript0222@gmail.com>
2025-04-23 21:55:16 +0500

    start

---
+++ src/main/java/com/bankofrhaen/appbankapplication/AppBankApplication.java
@@ -0,0 +1,13 @@
+package com.bankofrhaen.appbankapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankofrhaen/appbankapplication/AppBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankofrhaen.appbankapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
